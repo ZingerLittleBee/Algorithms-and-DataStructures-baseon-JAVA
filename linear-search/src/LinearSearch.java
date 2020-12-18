de3b87df9534d00c92607ea84d9d45b8d8b421c2
beeij <xyijmn@outlook.com>
2020-12-19 01:01:36 +0800

    Student 类的 Comparable 接口实现

--- linear-search/src/LinearSearch.java
+++ linear-search/src/LinearSearch.java
@@ -1,7 +1,7 @@
 /**
  * @Author: ye
  * @Date: 2020-12-13 22:55
- * @Description:
+ * @Description: 线性查找
  */
 public class LinearSearch {
     public LinearSearch() {}
@@ -15,9 +15,26 @@ public class LinearSearch {
     }
 
     public static void main(String[] args) {
-        Integer[] arr = {13, 5, 67, 3, 6, 73, 63, 22};
-        int search = LinearSearch.search(arr, 3);
-        System.out.println(search);
-    }
+//        Integer[] arr = {13, 5, 67, 3, 6, 73, 63, 22};
+//        int search = LinearSearch.search(arr, 3);
+//        System.out.println(search);
 
+        // 自定义类的线性查找
+//        Student[] students = {
+//                new Student("zs"),
+//                new Student("ls")};
+//        Student ls = new Student("ls");
+//        int isSearch = LinearSearch.search(students, ls);
+//        System.out.println(isSearch);
+
+        // 自定义类的排序
+        Student[] students = {
+                new Student("zs", 90),
+                new Student("ls", 88)};
+        Student ls = new Student("ls", 99);
+        SelectionSort.sort(students);
+        for (Student student : students) {
+            System.out.print(student + " ");
+        }
+    }
 }
--- linear-search/src/Student.java
+++ linear-search/src/Student.java
@@ -3,11 +3,13 @@
  * @Date: 2020-12-13 23:04
  * @Description:
  */
-public class Student {
+public class Student implements Comparable<Student>{
     private String name;
+    private Integer score;
 
-    Student(String name) {
+    Student(String name, Integer score) {
         this.name = name;
+        this.score = score;
     }
 
     @Override
@@ -25,12 +27,13 @@ public class Student {
         return name.toLowerCase().equals(temp.name.toLowerCase());
     }
 
-    public static void main(String[] args) {
-        Student[] students = {
-                new Student("zs"),
-                new Student("ls")};
-        Student ls = new Student("ls");
-        int isSearch = LinearSearch.search(students, ls);
-        System.out.println(isSearch);
+    @Override
+    public int compareTo(Student o) {
+        return this.score - o.score;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student(name: %s, score: %d)", name, score);
     }
 }
